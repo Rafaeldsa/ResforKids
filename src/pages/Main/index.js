2207d1c75c86d1940b08fc1f61c2917a7613b097
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-06-16 20:41:24 -0300

    up

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,59 @@
 import React from "react";
+import "./styles.css";
+import { FiArrowDownCircle, FiArrowLeft, FiArrowRight } from "react-icons/fi";
+import torre from "../../assets/torre.png";
+import { Player } from "video-react";
 
 const Main = () => {
   return (
     <div id="container">
-      <h1>ResForKids</h1>
+      <header>
+        <h1>RES for Kids</h1>
+      </header>
+
+      <div id="conteudo">
+        <Player>
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+        </Player>
+        <form action="">
+          <h1>QUIZ</h1>
+          <h2>Aqui vai a pergunta</h2>
+          <ul className="items-grid">
+            <li>
+              <img src={torre} />
+              <br />
+              <span>Torre</span>
+            </li>
+            <li>
+              <img src={torre} />
+              <br />
+              <span>Torre</span>
+            </li>
+            <li>
+              <img src={torre} />
+              <br />
+              <span>Torre</span>
+            </li>
+            <li>
+              <img src={torre} />
+              <br />
+              <span>Torre</span>
+            </li>
+          </ul>
+          <button>
+            <span>
+              <FiArrowLeft />
+            </span>
+            <strong>Anterior</strong>
+          </button>
+          <button>
+            <strong>Proxima</strong>
+            <span>
+              <FiArrowRight />
+            </span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };

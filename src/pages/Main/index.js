254262c656c964ac59a33f25b57e7e20e8b4d70e
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-06-26 18:55:26 -0300

    responsividade

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,9 +3,8 @@ import "./styles.css";
 import { FiArrowDownCircle, FiArrowLeft, FiArrowRight } from "react-icons/fi";
 import axios from "axios";
 import torre from "../../assets/torre.png";
-import ResponsivePlayer from "../../Components/ResponsivePlayer";
 
-import FileDownload from "js-file-download";
+import ResponsivePlayer from "../../Components/ResponsivePlayer";
 
 const Main = () => {
   function download() {
@@ -30,48 +29,52 @@ const Main = () => {
       </header>
 
       <div id="conteudo">
-        <ResponsivePlayer url="https://www.youtube.com/watch?v=4NRXx6U8ABQ" />
-        <form action="">
-          <h1>QUIZ</h1>
-          <h2>Aqui vai a pergunta</h2>
-          <ul className="items-grid">
-            <li>
-              <img src={torre} />
-              <br />
-              <span>Torre</span>
-            </li>
-            <li>
-              <img src={torre} />
-              <br />
-              <span>Torre</span>
-            </li>
-            <li>
-              <img src={torre} />
-              <br />
-              <span>Torre</span>
-            </li>
-            <li>
-              <img src={torre} />
-              <br />
-              <span>Torre</span>
-            </li>
-          </ul>
-          <button>
-            <span>
-              <FiArrowLeft />
-            </span>
-            <strong>Anterior</strong>
-          </button>
-          <button>
-            <strong>Proxima</strong>
-            <span>
-              <FiArrowRight />
-            </span>
-          </button>
-        </form>
+        <div className="container">
+          <div className="item1">
+            <button id="Beolica"></button>
+            <a href="#eolica" id="Teolica">
+              Energia Eólica
+            </a>
+          </div>
+          <div className="item2">
+            <button id="Bsolar"></button>
+            <a href="#solar" id="Tsolar">
+              Energia Solar
+            </a>
+          </div>
+        </div>
 
-        <button onClick={download}>
-          <strong>Download</strong>
+        <h1 id="eolica">Energia Eólica</h1>
+        <ResponsivePlayer url="https://www.youtube.com/watch?v=4NRXx6U8ABQ" />
+
+        <h1 id="quiz">Quiz</h1>
+
+        <div className="containerPergunta">
+          <div id="item1"></div>
+          <div id="item2"></div>
+        </div>
+
+        <div class="containerResposta">
+          <label for="01">
+            <input type="radio" name="sexo" id="01" value="01" />
+            <img src={torre} alt="Masculino" />
+          </label>
+          <label for="02">
+            <input type="radio" name="sexo" id="02" value="02" />
+            <img src={torre} alt="Feminino" />
+          </label>
+          <label for="03">
+            <input type="radio" name="sexo" id="03" value="03" />
+            <img src={torre} alt="Masculino" />
+          </label>
+          <label for="04">
+            <input type="radio" name="sexo" id="04" value="04" />
+            <img src={torre} alt="Feminino" />
+          </label>
+        </div>
+
+        <button id="download" onClick={download}>
+          <strong>Desenho_01</strong>
           <FiArrowDownCircle />
         </button>
       </div>

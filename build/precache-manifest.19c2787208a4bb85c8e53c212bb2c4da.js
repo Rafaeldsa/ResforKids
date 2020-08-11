597032002cd3e0d92e9f4e9cbab24ff84ab36c0d
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-08-10 21:27:35 -0300

    build

---
+++ build/precache-manifest.19c2787208a4bb85c8e53c212bb2c4da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da7f6ca58d35a70d7a8565c5d5329e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f045006c35c145e86bc3",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e5a5ed6a5aa089e25657",
+    "url": "/static/css/main.d3184514.chunk.css"
+  },
+  {
+    "revision": "f045006c35c145e86bc3",
+    "url": "/static/js/2.ee627a98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ee627a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f037e4429dbb9cada5",
+    "url": "/static/js/3.69f2b88c.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.69f2b88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a5ed6a5aa089e25657",
+    "url": "/static/js/main.05f9e39f.chunk.js"
+  },
+  {
+    "revision": "fa6b2b60cd7131903420",
+    "url": "/static/js/runtime-main.40f0535f.js"
+  },
+  {
+    "revision": "0cb7cf3f849f07bdfc14e74e2a3f7fbe",
+    "url": "/static/media/whatever.0cb7cf3f.mp3"
+  }
+]);
